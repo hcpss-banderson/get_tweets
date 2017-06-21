3cf0e9f62195150c223f8fdc85afb8513e54bcba
kapil.ropalekar <kapil.ropalekar@2393402.no-reply.drupal.org>
2017-06-21 16:04:10 +0300

    Issue #2888092 by kapil.ropalekar: $this->t() should be used instead of t() for Drupal 8 version

--- src/Form/GetTweetsSettings.php
+++ src/Form/GetTweetsSettings.php
@@ -34,7 +34,7 @@ class GetTweetsSettings extends ConfigFormBase {
 
     $form['import'] = [
       '#type' => 'checkbox',
-      '#title' => t('Import tweets'),
+      '#title' => $this->t('Import tweets'),
       '#default_value' => $config->get('import'),
     ];
 
@@ -56,33 +56,33 @@ class GetTweetsSettings extends ConfigFormBase {
     $intervals = [604800, 2592000, 7776000, 31536000];
     $form['expire'] = [
       '#type' => 'select',
-      '#title' => t('Delete old statuses'),
+      '#title' => $this->t('Delete old statuses'),
       '#default_value' => $config->get('expire'),
-      '#options' => [0 => t('Never')] + array_map([\Drupal::service('date.formatter'), 'formatInterval'], array_combine($intervals, $intervals)),
+      '#options' => [0 => $this->t('Never')] + array_map([\Drupal::service('date.formatter'), 'formatInterval'], array_combine($intervals, $intervals)),
     ];
 
     $form['oauth'] = [
       '#type' => 'fieldset',
-      '#title' => t('OAuth Settings'),
+      '#title' => $this->t('OAuth Settings'),
       '#description' => $this->t('To enable OAuth based access for twitter, you must <a href="@url">register your application</a> with Twitter and add the provided keys here.', ['@url' => 'https://dev.twitter.com/apps/new']),
     ];
 
     $form['oauth']['callback_url'] = [
       '#type' => 'item',
-      '#title' => t('Callback URL'),
+      '#title' => $this->t('Callback URL'),
       '#markup' => Url::fromUri('base:twitter/oauth', ['absolute' => TRUE])->toString(),
     ];
 
     $form['oauth']['consumer_key'] = [
       '#type' => 'textfield',
-      '#title' => t('OAuth Consumer key'),
+      '#title' => $this->t('OAuth Consumer key'),
       '#default_value' => $config->get('consumer_key'),
       '#required' => TRUE,
     ];
 
     $form['oauth']['consumer_secret'] = [
       '#type' => 'textfield',
-      '#title' => t('OAuth Consumer secret'),
+      '#title' => $this->t('OAuth Consumer secret'),
       '#default_value' => $config->get('consumer_secret'),
       '#required' => TRUE,
     ];
